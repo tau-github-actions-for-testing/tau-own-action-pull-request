789528fe1073abbd3b0fd2f3c16f29640a911f1a
Matthias-Zax <matthiaszax@gmail.com>
2023-03-10 09:06:25 +0100

    fix multiple lines

--- dist/index.js
+++ dist/index.js
@@ -9763,6 +9763,14 @@ const main = async () => {
             labels: ['yaml'],
           });
           break;
+        case 'html':
+          await octokit.rest.issues.addLabels({
+            owner,
+            repo,
+            issue_number: pr_number,
+            labels: ['html'],
+          });
+          break;
         default:
           await octokit.rest.issues.addLabels({
             owner,
